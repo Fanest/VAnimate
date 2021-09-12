0253550e80368693b9e21e4c94ff9d962307cf82
Fanest <stefan.the.mailer@gmail.com>
2021-09-12 23:57:23 +0200

    Added PolygonUtils.cs

---
+++ VAnimate/Utility/PolygonUtils.cs
@@ -0,0 +1,13 @@
+using System;
+using VAnimate.Data;
+
+namespace VAnimate.Utility
+{
+    public class PolygonUtils
+    {
+        public static Polygon AnimationFromSimpleMatrix((double? x, double? y)[,] keyframeMatrix, bool cycling, string uniqueId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
